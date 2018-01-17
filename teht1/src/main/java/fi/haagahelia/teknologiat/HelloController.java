bd349eed063598960825feb02747ac310b3f5730
Mark Laatikainen <mark@DESKTOP-ELM7KUE.lan>
2018-01-17 18:39:51 +0200

    Tehtävä1

---
+++ teht1/src/main/java/fi/haagahelia/teknologiat/HelloController.java
@@ -0,0 +1,33 @@
+package fi.haagahelia.teknologiat;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HelloController {
+
+	@RequestMapping("/")
+	public @ResponseBody String helloWorld() {
+		
+		return "Hello Spring";
+	}
+	
+	@RequestMapping("/hello")
+	public @ResponseBody String hello(@RequestParam("location") String location, @RequestParam("name") String name) {
+		return "Welcome to the " + location + " " + name + "!";
+	}
+
+	@RequestMapping("/index")
+	public String index() {
+		return "This is the main page";
+	}
+
+	@RequestMapping("/contact")
+	public String contact() {
+		return "This is the contact page";
+	}
+
+}
